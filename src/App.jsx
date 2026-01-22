a0d5138da4911a8016e81fa7798d9ec0c1236f33
Smurfish-py <m.rifki0729@gmail.com>
2026-01-22 12:35:50 +0700

    feat: refactor App component to use React Router for navigation and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MainLayouts from "@/layouts/MainLayouts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { 
+  Home, Login 
+} from "@/pages/index";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayouts />}>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/notification" />
+          <Route path="/report" />
+          <Route path="/admin/logs" />
+          <Route path="/admin/requests"  />
+        </Route>
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
