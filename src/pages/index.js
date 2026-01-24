c40cb474e79f478f73971ff060a82f694cfdf0c4
Smurfish-py <m.rifki0729@gmail.com>
2026-01-24 09:54:36 +0700

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from '@/pages/Home';
 export { default as Login } from '@/pages/Login';
+export { default as LogPage } from '@/pages/LogPage';
+export { default as RequestPage } from '@/pages/RequestPage';
