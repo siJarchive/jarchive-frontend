bf478accedfa9ed389f406db75f078373b271f9f
Smurfish-py <m.rifki0729@gmail.com>
2026-01-24 09:53:56 +0700

    Adding function fetchAssets,     uploadAsset,     updateAsset,     deleteAsset,     requestFile,     fetchRequests,     approveRequest,     rejectRequest

---
+++ src/controller/file.controller.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const fetchAssets = async (pageNum = 1, search, category, sort) => {
+    try {
+        const res = await axios.get(`${API_URL}/api/assets`, {
+            params: {
+                category,
+                sort,
+                search,
+                page: pageNum
+            }
+        });
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const uploadAsset = async (formData) => {
+    try {
+        const res = await axios.post(`${API_URL}/api/upload`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateAsset = async (id, data) => {
+    try {
+        const res = await axios.put(`${API_URL}/api/assets/${id}`, data);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteAsset = async (id) => {
+    try {
+        const res = await axios.delete(`${API_URL}/api/assets/${id}`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const requestFile = async (formData) => {
+    try {
+        const res = await axios.post(`${API_URL}/api/requests`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const fetchRequests = async () => {
+    try {
+        const res = await axios.get(`${API_URL}/api/requests`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const approveRequest = async (id) => {
+    try {
+        const res = await axios.post(`${API_URL}/api/requests/${id}/approve`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const rejectRequest = async (id) => {
+    try {
+        const res = await axios.post(`${API_URL}/api/requests/${id}/reject`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export {
+    fetchAssets,
+    uploadAsset,
+    updateAsset,
+    deleteAsset,
+    requestFile,
+    fetchRequests,
+    approveRequest,
+    rejectRequest
+}
