9d541d7da8dab440f4062ce3249557e27ede7ac8
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-02-12 15:31:58 +0700

    refactor: update routing structure and component imports in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import MainLayouts from "@/layouts/MainLayouts";
+import HomeLayouts from "@/layouts/HomeLayouts";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { 
-  Home, Login, LogPage, RequestPage
+  HomePage, Dashboard, Login, LogPage, RequestPage
 } from "@/pages/index";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<HomeLayouts />} >
+          <Route path="/" element={<HomePage />} />
+        </Route>
         <Route element={<MainLayouts />}>
-          <Route path="/" element={ <Home /> } />
+          <Route path="/dashboard" element={ <Dashboard /> } />
           <Route path="/notification" />
           <Route path="/report" />
           <Route path="/admin/logs" element={ <LogPage /> } />
