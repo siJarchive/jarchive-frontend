90d1c680f79bc8252cc409a7c226ef222114a653
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-02-12 15:36:49 +0700

    fix: update export for HomePage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-export { default as Home } from '@/pages/Home';
+export { default as HomePage } from '@/pages/HomePage';
+export { default as Dashboard } from '@/pages/Dashboard';
 export { default as Login } from '@/pages/Login';
 export { default as LogPage } from '@/pages/LogPage';
 export { default as RequestPage } from '@/pages/RequestPage';
