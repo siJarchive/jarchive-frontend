df1a4aa10b56602ce23f2e010283942ff7387f8a
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-02-12 15:35:34 +0700

    Adding export FileCardExample

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from '@/components/Header';
 export { default as Sidebar } from '@/components/Sidebar';
 export { default as FileCard } from '@/components/FileCard';
+export { default as FileCardExample } from '@/components/FileCardExample';
